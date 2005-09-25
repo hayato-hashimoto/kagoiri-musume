a1891247e1a048d8acbf6f7a586f116b21c91b76
shibata <shibata@4e392e78-dc3f-dc11-8fd5-00188bfc9ac4>
2005-09-25 13:36:42 +0000

    add search entry.
    git-svn-id: http://svn.kahua.org/kagoiri-musume/trunk@979 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- plugins/kagoiri-musume.scm
+++ plugins/kagoiri-musume.scm
@@ -1,6 +1,7 @@
 (allow-module gauche.charconv)
 (allow-module gauche.logger)
 (allow-module rfc.base64)
+(allow-module srfi-1)
 (allow-module srfi-13)
 (allow-module srfi-19)
 (allow-module util.list)
