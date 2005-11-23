2d818f823fcb0889b9663f2906f5b088b5bfa772
shibata <shibata@4e392e78-dc3f-dc11-8fd5-00188bfc9ac4>
2005-11-23 04:37:32 +0000

    added rfc.uri to allow-module removed rfc.base64 from allow-module
    git-svn-id: http://svn.kahua.org/kagoiri-musume/trunk@1277 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- plugins/kagoiri-musume.scm
+++ plugins/kagoiri-musume.scm
@@ -3,12 +3,12 @@
 ;;  Copyright (c) 2005 Kahua.Org, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: kagoiri-musume.scm,v 1.14 2005/11/08 12:35:37 cut-sea Exp $
+;; $Id: kagoiri-musume.scm,v 1.15 2005/11/23 04:37:32 shibata Exp $
 
 (allow-module gauche.charconv)
 (allow-module gauche.logger)
 (allow-module file.util)
-(allow-module rfc.base64)
+(allow-module rfc.uri)
 (allow-module srfi-1)
 (allow-module srfi-10)
 (allow-module srfi-13)
