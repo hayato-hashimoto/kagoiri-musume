64171b200a8cae2b5f2daa0245a76da0c74d0ad6
shibata <shibata@4e392e78-dc3f-dc11-8fd5-00188bfc9ac4>
2006-02-18 12:52:44 +0000

    Rewrite test.
    git-svn-id: http://svn.kahua.org/kagoiri-musume/trunk@1621 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

---
+++ test/unit-list.scm
@@ -0,0 +1,23 @@
+;; -*- coding: euc-jp; mode: scheme -*-
+;;
+;;  Copyright (c) 2005 Kahua.Org, All rights reserved.
+;;  See COPYING for terms and conditions of using this software
+;;
+;; $Id: unit-list.scm,v 1.1 2006/02/18 12:52:44 shibata Exp $
+
+(use gauche.test)
+(use gauche.collection)
+(use file.util)
+(use text.tree)
+(use sxml.ssax)
+(use sxml.sxpath)
+(use kahua)
+(use kahua.test.xml)
+(use kahua.test.worker)
+
+(load "common.scm")
+
+(test-start "kagoiri-musume unit-list enter check")
+
+(*setup*)
+(test-end)
