5cf28149549e3c3c5b6a7b5aa8547cacc072019d
shibata <shibata@4e392e78-dc3f-dc11-8fd5-00188bfc9ac4>
2006-03-11 08:48:29 +0000

    Added `(allow-module gauche.version)'. (for migration.kahua)
    git-svn-id: http://svn.kahua.org/kagoiri-musume/trunk@1732 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- plugins/kagoiri-musume.scm
+++ plugins/kagoiri-musume.scm
@@ -3,7 +3,7 @@
 ;;  Copyright (c) 2005 Kahua.Org, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: kagoiri-musume.scm,v 1.17 2005/11/30 13:11:12 cut-sea Exp $
+;; $Id: kagoiri-musume.scm,v 1.18 2006/03/11 08:48:29 shibata Exp $
 
 ;; for debug
 ;(allow-module gauche.interactive)
@@ -24,6 +24,7 @@
 (allow-module gauche.sequence)
 (allow-module gauche.parameter)
 (allow-module gauche.vm.debugger)
+(allow-module gauche.version)
 (allow-module text.gettext)
 
 
