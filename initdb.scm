4189d0e48b8c9636b8d4061dfd7bc16c795c224d
cut-sea <cut-sea@4e392e78-dc3f-dc11-8fd5-00188bfc9ac4>
2006-12-27 17:45:52 +0000

    2006-12-28  Katsutoshi Itoh <cut-sea@kahua.org>
            * modify: catch up with Kahua-1.0 site bundle.
    
    
    git-svn-id: http://svn.kahua.org/kagoiri-musume/trunk@2438 4e392e78-dc3f-dc11-8fd5-00188bfc9ac4

--- initdb.scm
+++ initdb.scm
@@ -3,11 +3,12 @@
 ;;  Copyright (c) 2005 Kahua.Org, All rights reserved.
 ;;  See COPYING for terms and conditions of using this software
 ;;
-;; $Id: initdb.scm,v 1.23 2006/12/14 06:35:40 cut-sea Exp $
+;; $Id: initdb.scm,v 1.24 2006/12/27 17:45:52 cut-sea Exp $
 
 ;;
 ;; include
 (use file.util)
+(use gauche.parseopt)
 (use kahua)
 (use kahua-server)
 
@@ -21,12 +22,10 @@
 
 ;; DB Setting.
 ;;
-(define-if-not-bound *kagoiri-musume-database-name*
-  #`"efs:,(kahua-working-directory)/kagoiri-musume")
 
 (define-if-not-bound *kagoiri-musume-log-database-name*
-  #`",(kahua-working-directory)/kagoiri-musume/log")
-
+  (let1 conf (kahua-config)
+    #`",(ref conf 'database-directory)/log"))
 
 ;; tip
 (define-macro (new class key . args)
@@ -40,34 +39,38 @@
 
 ;;
 (define (main args)
-  (with-db (db *kagoiri-musume-database-name*)
-    (new-fan "kago" "kago" "cut-sea@kagoiri.org" 'admin 'developer 'client 'user)
-    (new-fan "cut-sea" "cutsea" "cut-sea@kagoiri.org" 'developer 'user)
+  (let-args (cdr args)
+      ((site "S=s" #f))
+    (let* ((conf (if site (kahua-common-init site #t) (kahua-config)))
+	   (database  #`",(ref conf 'database-directory)/,(ref conf 'default-database-name)"))
+      (with-db (db database)
+	  (new-fan "kago" "kago" "cut-sea@kagoiri.org" 'admin 'developer 'client 'user)
+	(new-fan "cut-sea" "cutsea" "cut-sea@kagoiri.org" 'developer 'user)
 
-    (new <priority> "normal" :disp-name "普通" :level 3)
-    (new <priority> "low" :disp-name "低" :level 2)
-    (new <priority> "high" :disp-name "高" :level 4)
-    (new <priority> "super" :disp-name "超高" :level 5)
+	(new <priority> "normal" :disp-name "普通" :level 3)
+	(new <priority> "low" :disp-name "低" :level 2)
+	(new <priority> "high" :disp-name "高" :level 4)
+	(new <priority> "super" :disp-name "超高" :level 5)
 
-    (new <status> "open" :disp-name "OPEN")
-    (new <status> "completed" :disp-name "COMPLETED")
-    (new <status> "on-hold" :disp-name "ON HOLD")
-    (new <status> "taken" :disp-name "TAKEN")
-    (new <status> "rejected" :disp-name "REJECTED")
+	(new <status> "open" :disp-name "OPEN")
+	(new <status> "completed" :disp-name "COMPLETED")
+	(new <status> "on-hold" :disp-name "ON HOLD")
+	(new <status> "taken" :disp-name "TAKEN")
+	(new <status> "rejected" :disp-name "REJECTED")
 
-    (new <type> "bug" :disp-name "バグ")
-    (new <type> "task" :disp-name "タスク")
-    (new <type> "request" :disp-name "変更要望")
-    (new <type> "discuss" :disp-name "議論")
-    (new <type> "report" :disp-name "報告")
-    (new <type> "term" :disp-name "用語")
-    (new <type> "etc" :disp-name "その他")
+	(new <type> "bug" :disp-name "バグ")
+	(new <type> "task" :disp-name "タスク")
+	(new <type> "request" :disp-name "変更要望")
+	(new <type> "discuss" :disp-name "議論")
+	(new <type> "report" :disp-name "報告")
+	(new <type> "term" :disp-name "用語")
+	(new <type> "etc" :disp-name "その他")
 
-    (new <category> "section" :disp-name "セクション")
-    (new <category> "global" :disp-name "全体")
-    (new <category> "infra" :disp-name "インフラ")
-    (new <category> "master" :disp-name "マスタ")
-    )
-  (format #t "database initialize done~%")
-  (exit 0))
+	(new <category> "section" :disp-name "セクション")
+	(new <category> "global" :disp-name "全体")
+	(new <category> "infra" :disp-name "インフラ")
+	(new <category> "master" :disp-name "マスタ")
+	)
+      (format #t "database initialize done~%")
+      (exit 0))))
 
